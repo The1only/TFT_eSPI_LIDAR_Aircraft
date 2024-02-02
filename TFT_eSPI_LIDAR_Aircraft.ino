2f76781596998dc2b25e53e07450fc7fd5b9828c
The1only <teni@9tek.no>
2024-02-02 23:16:30 +0100

    Added some more RTC and SD functions...

--- TFT_eSPI_LIDAR_Aircraft.ino
+++ TFT_eSPI_LIDAR_Aircraft.ino
@@ -15,8 +15,8 @@
 * This project and all its code is fri to use, but send pull request with updates.
 * However; if it is used comersially, my name shall be listed.
 **********************************************************************************/
-#define Use_TFT       // Some might only whant to use an Androiud app etc. Then the Sircular display can be removed. 
-#undef USE_LIGHT      // If an relay for warning light etc, is mounted...
+#define Use_TFT   // Some might only whant to use an Androiud app etc. Then the Sircular display can be removed.
+#undef USE_LIGHT  // If an relay for warning light etc, is mounted...
 
 #define DISABLE_ALL_LIBRARY_WARNINGS  // Hmm well for now...
 #define USE_TFT_ESPI_LIBRARY          // Yep we do...
@@ -31,7 +31,8 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LIDARLite.h>
-#include "I2C_BM8563.h"               // RTC driver...
+#include <SD.h>
+#include "I2C_BM8563.h"  // RTC driver...
 #include "NotoSansBold15.h"
 
 // YES we may also suport ESP32 and add w-lan and bluetooth... (Not tested)
@@ -44,7 +45,10 @@ const char *password = "mypassword";
 #endif
 
 #define OFFSET 1.7   // The sensor is mounted 1.7' abow ground when the rear wheels touches ground... (Must be set individually)
-#define MAX_DIST 16  // 16 feet = 5 meters... 
+#define MAX_DIST 16  // 16 feet = 5 meters...
+
+#define NUM_ADC_SAMPLE 20
+#define RP2040_VREF 3300  // The actual voltage on 3V3 pin. (unit: mV)
 
 LIDARLite myLidarLite;
 const int ledPin = LED_BUILTIN;  // the number of the LED/Relay pin... (If used)
@@ -61,11 +65,12 @@ lv_coord_t last_touchX, last_touchY;
 #endif
 
 int flash = 0;
-int mode = 0;               // We support Mode 0 and Mode 1 (just screen layouts...)
-int modepressed = 0;        // It is a bit tricky if someone press the screen for longer time, so we fix that... 
+int mode = 0;         // We support Mode 0 and Mode 1 (just screen layouts...)
+int modepressed = 0;  // It is a bit tricky if someone press the screen for longer time, so we fix that...
 int cal_cnt = 0;
+static lv_obj_t *battery_bar, *battery_label;
 
-#define CLOCK_X_POS 10      // X, Y position of screen preasure, can be used to identify location...
+#define CLOCK_X_POS 10  // X, Y position of screen preasure, can be used to identify location...
 #define CLOCK_Y_POS 10
 
 /***************************************************************************************
@@ -128,15 +133,46 @@ float time_secs = h * 3600 + m * 60 + s;
 
 // Time for next tick
 uint32_t targetTime = 0;
-
 const int Val[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+// Log every 1/10 sec. when in green zone... and when going from green to read.
+// Stop loggin in black zone for 30sec., and when in red zoone for 1 minute.
+// Stop logging = Write to SD card...
+static uint16_t log_data[6500];  // 10min = 600sec = 6000samples  feet * 10 ...   10 = 1.0  255 = 25.5 feet...
+
+// Log takeoff when going from red for 5sec. ...
+// Log landed when 30sec. consecutive in read zone...
+// Write to SD when landed...
+I2C_BM8563_TimeTypeDef Takeoff_timeStruct;
+I2C_BM8563_DateTypeDef Takeoff_dateStruct;
+I2C_BM8563_TimeTypeDef Land_timeStruct;
+I2C_BM8563_DateTypeDef Land_dateStruct;
+
+// =========================================================================
+bool getLocalTime(struct tm *timeInfo) {
+  timeInfo->tm_hour = 22;
+  timeInfo->tm_min = 23;
+  timeInfo->tm_sec = 1;
+
+  timeInfo->tm_wday = 5;
+  timeInfo->tm_mon = 2;
+  timeInfo->tm_mday = 2;
+  timeInfo->tm_year = 2024;
+  return true;
+}
 
 // =========================================================================
 // Setup
 // =========================================================================
 void setup() {
   Serial.begin(115200);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB
+  }
+
+  Serial.println("Altitude Indicator v1.0");
   Serial.println("Booting...");
+
 
   // Initialise the screen
 #ifdef Use_TFT
@@ -145,7 +181,6 @@ void setup() {
   lv_xiao_touch_init();
   tft.init();
   pinMode(TOUCH_INT, INPUT_PULLUP);
-  Wire.begin();
 
   // Ideally set orientation for good viewing angle range because
   // the anti-aliasing effectiveness varies with screen viewing angle
@@ -159,10 +194,62 @@ void setup() {
 
   // Only 1 font used in the sprite, so can remain loaded
   face.loadFont("NotoSansBold15");
+  face.setTextDatum(MC_DATUM);
+  face.setTextColor(CLOCK_FG, CLOCK_BG);
+  face.setTextSize(2);
+  face.drawString("Altitude Indicator", CLOCK_R, CLOCK_R * 0.7);
+  face.drawString("v1.0", CLOCK_R, CLOCK_R);
+  face.pushSprite(5, 5, TFT_TRANSPARENT);
+  delay(2000);
 
   // Draw the whole clock - NTP time not available yet
   renderFace(0);
 #endif
+
+  // RTC...
+  struct tm timeInfo;
+  Wire.begin();
+
+  getLocalTime(&timeInfo);
+    timeStruct.hours = timeInfo.tm_hour;
+  timeStruct.minutes = timeInfo.tm_min;
+  timeStruct.seconds = timeInfo.tm_sec;
+  rtc.setTime(&timeStruct);
+
+  dateStruct.weekDay = timeInfo.tm_wday;
+  dateStruct.month = timeInfo.tm_mon;
+  dateStruct.date = timeInfo.tm_mday;
+  dateStruct.year = timeInfo.tm_year;
+  rtc.setDate(&dateStruct);
+
+  Serial.printf("->%04d/%02d/%02d %02d:%02d:%02d\n",
+                dateStruct.year,
+                dateStruct.month,
+                dateStruct.date,
+                timeStruct.hours,
+                timeStruct.minutes,
+                timeStruct.seconds);
+
+
+  // We do not use time at the moment, but can be used for automatic recoding of flight time etc...
+  // Get RTC
+  rtc.getDate(&dateStruct);
+  rtc.getTime(&timeStruct);
+
+  // Print RTC
+  Serial.printf("%04d/%02d/%02d %02d:%02d:%02d\n",
+                dateStruct.year,
+                dateStruct.month,
+                dateStruct.date,
+                timeStruct.hours,
+                timeStruct.minutes,
+                timeStruct.seconds);
+
+  rtc.begin();
+  syncTime();
+
+  Serial.print("Battery level: ");
+  Serial.println(battery_level_percent());
 
   myLidarLite.begin(0, true);  // Set configuration to default and I2C to 400 kHz
   pinMode(ledPin, OUTPUT);
@@ -177,23 +264,33 @@ void setup() {
   configTime(8 * 3600, 0, ntpServer);
 #endif
 
-  // We do not use time at the moment, but can be used for automatic recoding of flight time etc... 
-  // Get RTC
-  rtc.getDate(&dateStruct);
-  rtc.getTime(&timeStruct);
+  /*
+  int attempts = 0;
+  int maxAttempts = 50;
+  int delayBetweenAttempts = 300;
+  bool isblinked = false;
 
-  // Print RTC
-  Serial.printf("%04d/%02d/%02d %02d:%02d:%02d\n",
-                dateStruct.year,
-                dateStruct.month,
-                dateStruct.date,
-                timeStruct.hours,
-                timeStruct.minutes,
-                timeStruct.seconds);
+  pinMode(D2, OUTPUT);
+  while(! SD.begin(D2) ) {
+    Serial.print("SD Card mount failed!");
+    isblinked = !isblinked;
+    attempts++;
+    if( isblinked ) {
+      tft.setTextColor( TFT_WHITE, TFT_BLACK );
+    } else {
+      tft.setTextColor( TFT_BLACK, TFT_WHITE );
+    }
+    tft.drawString( "INSERT SD", tft.width()/2, tft.height()/2 );
 
-//  Wire.begin();
-//  rtc.begin();
-//  syncTime();
+    if( attempts > maxAttempts ) {
+      Serial.print("Giving up");
+      break;
+    }
+    delay( delayBetweenAttempts );
+  }
+  SD.end();
+  Serial.print("SD Card mount done!");
+*/
 }
 
 // =========================================================================
@@ -237,10 +334,10 @@ void loop() {
 
   dist = dist / 914.399970739201;  // (3000.0/3.28084);  // Make avradge and make feet from cm...
   dist = dist - OFFSET;
-  if (dist < 0) dist = 0;                // Limmit distance to max feet...
+  if (dist < 0) dist = 0;  // Limmit distance to max feet...
 
-  Serial.print("dist=");
-  Serial.println(dist);
+    //  Serial.print("dist=");
+    //  Serial.println(dist);
 
 #ifdef USE_LIGHT
   // Drive the alarm LED...
@@ -413,6 +510,78 @@ static void renderFace2(float t) {
   face.pushSprite(5, 5, TFT_TRANSPARENT);
 }
 #endif
+
+// =========================================================================
+
+static void *OpenFile(const char *fname, int32_t *pSize) {
+  static File FSGifFile;  // temp gif file holder
+  FSGifFile = SD.open(fname);
+  if (FSGifFile) {
+    *pSize = FSGifFile.size();
+    return (File *)&FSGifFile;
+  }
+  return NULL;
+}
+
+// =========================================================================
+
+static void CloseFile(File *pHandle) {
+  if (pHandle != NULL)
+    pHandle->close();
+}
+
+// =========================================================================
+
+static int32_t ReadFile(File *pFile, uint8_t *pBuf, int32_t iLen, int32_t *pos) {
+  int32_t iBytesRead = (int32_t)pFile->read(pBuf, iLen);
+  *pos = pFile->position();
+  return iBytesRead;
+}
+
+// =========================================================================
+
+static int32_t WriteFile(File *pFile, uint8_t *pBuf, int32_t iLen, int32_t *pos) {
+  int32_t iBytesWritten = (int32_t)pFile->write(pBuf, iLen);
+  *pos = pFile->position();
+  return iBytesWritten;
+}
+
+// =========================================================================
+
+static int32_t SeekFile(File *pFile, int32_t iPosition) {
+  pFile->seek(iPosition);
+  return (int32_t)pFile->position();
+}
+
+// =========================================================================
+
+int32_t battery_level_percent(void) {
+  int32_t mvolts = 0;
+#if defined(CONFIG_IDF_TARGET_ESP32S3) || defined(CONFIG_IDF_TARGET_ESP32C3)
+  for (int8_t i = 0; i < NUM_ADC_SAMPLE; i++) {
+    mvolts += analogReadMilliVolts(D0);
+  }
+  mvolts /= NUM_ADC_SAMPLE;
+#elif defined(ARDUINO_SEEED_XIAO_NRF52840_SENSE) || defined(ARDUINO_SEEED_XIAO_NRF52840)
+  int32_t adc_raw = 0;
+  for (int8_t i = 0; i < NUM_ADC_SAMPLE; i++) {
+    adc_raw += analogRead(D0);
+  }
+  adc_raw /= NUM_ADC_SAMPLE;
+  mvolts = 2400 * adc_raw / (1 << 12);
+#elif defined(ARDUINO_SEEED_XIAO_RP2040)
+  int32_t adc_raw = 0;
+  for (int8_t i = 0; i < NUM_ADC_SAMPLE; i++) {
+    adc_raw += analogRead(D0);
+  }
+  adc_raw /= NUM_ADC_SAMPLE;
+  mvolts = RP2040_VREF * adc_raw / (1 << 12);
+#endif
+  int32_t level = (mvolts - 1850) * 100 / 250;  // 1850 ~ 2100
+  level = (level < 0) ? 0 : ((level > 100) ? 100 : level);
+  return level;
+}
+
 // =========================================================================
 // Get coordinates of end of a line, pivot at x,y, length r, angle a
 // =========================================================================
@@ -426,9 +595,7 @@ void getCoord(int16_t x, int16_t y, float *xp, float *yp, int16_t r, float a) {
 }
 
 void syncTime(void) {
-#ifdef Use_TFT
   targetTime = millis() + 100;
   rtc.getTime(&timeStruct);
   time_secs = timeStruct.hours * 3600 + timeStruct.minutes * 60 + timeStruct.seconds;
-#endif
 }
